efe2c72230424cbc9b4e488283a525026015c33b
Vladimir Chmykov <vov.chm@ya.ru>
2018-10-06 13:29:48 +0500

    implement binary search in python

---
+++ algorithms/binary_search/python/v_chmykov.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def binary_search(array, number):
+    length = len(array)
+    if length == 0:    
+        return False
+
+    low = 0
+    high = length - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if number < array[mid]:
+            high = mid - 1
+        elif number > array[mid]:
+            low = mid + 1
+        else:
+            return mid
+    else:
+        False
+
+def main():
+    test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    found_item = binary_search(test_array, 3)
+    print(found_item)
+ 
+main()
