1f74e14170a1114ae9e2012f33a08eebf7b5d8ac
Shakeel Samsudeen <shakeelsamsudeen15@gmail.com>
2018-10-06 08:38:48 -0500

    Added Hello World for Java (#107)
    * implemented binary search in java
    
    hacktoberfest
    
    * changed file location to match convention
    
    * created shakeel.java
    
    prints Hello World!
    
    * Delete shakeel.java
    
    * Update shakeel.java
    
    * added shakeel.java
    
    Hello World in Java
    hacktoberfest
    
    * Delete shakeel.java

---
+++ algorithms/hello_world/Java/shakeel.java
@@ -0,0 +1,11 @@
+/**
+ * @author: shakeelsamsu
+ * https://github.com/shakeelsamsu
+ * Prints Hello World
+ */
+
+public class shakeel {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
