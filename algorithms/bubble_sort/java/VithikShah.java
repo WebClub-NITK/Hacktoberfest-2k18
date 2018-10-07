fc9b0255e0f260bc9c54f2719c2db367a6132a12
VithikShah <32351341+VithikShah@users.noreply.github.com>
2018-10-07 15:22:17 +0530

    Create VithikShah.java

---
+++ algorithms/bubble_sort/java/VithikShah.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Bubble
+{
+public static void main(String args[])
+{
+int a=new int[100];
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number of elements");
+int n=sc.nextInt();
+
+
