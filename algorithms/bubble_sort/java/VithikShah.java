d0f2269cc2c7a6d15dc10a444c4b02b18c0f616f
VithikShah <32351341+VithikShah@users.noreply.github.com>
2018-10-07 15:27:47 +0530

    VithikShah.java
    BubbleSort in JAVA

--- algorithms/bubble_sort/java/VithikShah.java
+++ algorithms/bubble_sort/java/VithikShah.java
@@ -3,9 +3,35 @@ class Bubble
 {
 public static void main(String args[])
 {
+int temp=0;
 int a=new int[100];
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter number of elements");
 int n=sc.nextInt();
+  System.out.println("Enter elements");
+for(int i=0;i<n;i++)
+{
+  a[i]=sc.nextInt();
+}
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n-i-1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+        temp=a[j];
+        a[j]=a[j+1];
+        a[j+1]=temp;
+      }
+    }
+  }
+    
+    System.out.println("printing elements");
+for(int i=0;i<n;i++)
+{
+System.out.println(a[i]);
+}
+  }
+}
 
 
