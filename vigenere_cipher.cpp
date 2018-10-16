b40032eb2b74abdbcdc2e95e3192ba926a0724c9
VARUN GUPTA <varun.gpt1007@gmail.com>
2018-10-17 00:47:51 +0530

    VGIENERE CIPHER

---
+++ vigenere_cipher.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+string encrypt(string s,string key)
+{
+    long long int s1=s.size();
+    long long int s2=key.size(),j=0;
+    string res;
+    for(long long int i=0;i<s1;i++)
+    {
+      long long int p=s[i];
+      long long int k=key[j%s2];
+      j=j+1;
+      long long int r=(p+k)%26+65;
+      res.push_back(r);
+    }
+    return res;
+}
+string decrypt(string s,string key)
+{
+    long long int s1=s.size();
+    long long int s2=key.size(),j=0;
+    string res;
+    for(long long int i=0;i<s1;i++)
+    {
+      long long int p=s[i];
+      long long int k=key[j%s2];
+      j=j+1;
+      long long int r=(p-k+26)%26+65;
+      res.push_back(r);
+    }
+    return res;
+}
+int main()
+{
+	string s,k;
+    cout<<"Please enter the text to be encrypted:-"<<endl;
+    cin>>s;
+    cout<<"Please enter the KEY:-"<<endl;
+    cin>>k;
+    string cipher=encrypt(s,k);
+    cout<<"The encrypted cipher is:- "<<cipher<<endl;
+    cout<<"The decrypted message is:- "<<decrypt(cipher,k)<<endl;
+	return 0;
+}
